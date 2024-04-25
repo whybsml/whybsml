0da3409c4f13385e9307b53fb54ab17b44ccb1e1
Loulergue Frederic <frederic.loulergue@univ-orleans.fr>
2024-04-26 01:04:32 +0200

    Count application: improvements

--- application/count/count.ml
+++ application/count/count.ml
@@ -1,12 +1,38 @@
 let _ = Random.self_init ()
 
-let gen_matrix size = 
-  let mat = Array.make_matrix size size 0 in 
-  let column = Random.int size 
-  and row = Random.int size in
-  mat.(column).(row) <- 1;
+type matrix = int array array 
+
+let gen_matrix (size: int): matrix = 
+  let mat = Array.make_matrix size size 0 in
+  let set () = 
+    let column = Random.int size 
+    and row = Random.int size in
+    mat.(column).(row) <- 1 in
+  for i = 0 to Random.int size do 
+    set ();
+  done;
   mat 
 
+let is_square (m: matrix): bool =
+  let dim = Array.length m in 
+  Array.for_all (fun row->(Array.length row)=dim) m
+
+let dim(m: matrix): int = 
+  assert(is_square m);
+  Array.length m
+
+let ( *! ) (m1: matrix) (m2: matrix): matrix =
+  assert(is_square m1 && is_square m2 && dim m1 = dim m2);
+  let n = (dim m1) in 
+  let m = Array.make_matrix n n 0 in 
+  for i = 0 to n - 1 do 
+    for j = 0 to n - 1 do 
+      for k = 0 to n - 1 do 
+        m.(i).(j) <- m.(i).(j) + m1.(i).(k) * m2.(k).(j)
+      done;
+    done;
+  done;
+  m
 let is_diagonal mat = 
   let dim = Array.length mat in 
   assert(Array.for_all (fun row->(Array.length row)=dim) mat);
@@ -21,6 +47,6 @@ let is_diagonal mat =
 
 let _ = 
   Cli.run_homomorphism 
-    (fun _ ->gen_matrix 100)
-    (Count__Count.count_par is_diagonal)
+    (fun _ ->gen_matrix 50)
+    (Count__Count.count_par (fun m-> (m *! m) |> is_diagonal))
     Cli.print_Z
